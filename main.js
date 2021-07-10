94b5b1a665a5cd5b7696166e9068ec3ad3ab5db2
emquiroga <emiliano.quiroga093@gmail.com>
2021-07-10 10:53:07 -0300

    Update
    App working, ready for first styles approach, than came back for animations JS

--- main.js
+++ main.js
@@ -12,9 +12,11 @@ countdownDate.min = todayDate;
 countdownDate.value = todayDate;
 //Buttons
 let countdownBtn = document.getElementById("submit-button");
+let resetBtn = document.getElementById("reset-button");
 //Sections & display
 let formSection = document.getElementById("form-section");
 let countdownSection = document.getElementById("countdown-section");
+let mainTitle = document.getElementById("title");
 //Date array
 let countdownElements = ["days", "hours", "minutes", "seconds"];
 //Reset
@@ -24,16 +26,20 @@ console.log(resetDate);
 //Displaying sections and starting app
 countdownBtn.addEventListener('click', (e) => {
     e.preventDefault();//Prevent default reload page behavior
-    if (countdownDate.value && countdownTime.value) {
-        let selectedTime = new Date(`${countdownDate.value}T${countdownTime.value}:00`).getTime();
-        startCountdown (selectedTime);
-    }
+        if (countdownDate.value && countdownTime.value) {
+            formSection.style.display = 'none';//Hides the form section until reset
+            countdownSection.style.display = 'inherit';//Shows the countdown section after setting the date
+            resetBtn.style.display = 'visible';
+            let selectedTime = new Date(`${countdownDate.value}T${countdownTime.value}:00`).getTime();
+            mainTitle.textContent = `WE ARE LAUNCHING SOON!`;
+            startCountdown (selectedTime);
+        }
 })
 //Countdown function
 function startCountdown (date) {
     let x = setInterval(() => {countDown(date)}, 1000);
     let w = setInterval(() => {
-        //expired (x)
+        expired (x)
         if (resetDate) {
             clearInterval(x);
             resetDate = false;
@@ -65,7 +71,29 @@ function fillCard() {
     //Adding a "0" before seconds, minutes and hours if they're < 10
     for (let j = 1; j < countdownElements.length; j++) {
         if (time [j] < 10) {
-            document.getElementById (countdownElement.length[j]).innerHTML="0"+time[j];
+            document.getElementById (countdownElements[j]).innerHTML="0"+time[j];
         };
     };
 }
+//Reset app
+resetBtn.addEventListener('click', () => {
+    resetDate = true;
+    console.log(resetDate);
+    formSection.style.display = 'inherit';//Show again the form section
+    countdownSection.style.display = 'none';//Hide the countdown
+    return resetDate;
+})
+//Time expired
+function expired(x){
+    if (distance <= 0) {
+        clearInterval(x);//Finish countdown
+
+        //Fill cards with 00s
+        document.getElementById(countdownElements[0]).innerHTML = '0'; //Days
+        for (let i = 1; i < countdownElements.length; i++){
+            document.getElementById(countdownElements[i]).innerHTML = '00'; //Hours, minutes, seconds
+        }
+        //Change title content
+        mainTitle.textContent = `EXPIRED`;
+    }
+};
