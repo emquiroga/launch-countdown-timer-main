08ac53433cbe5b4c604446e9426d25ae71b6bab3
emquiroga <emiliano.quiroga093@gmail.com>
2021-07-21 13:20:23 -0300

    Finished
    So happy with this challenge! Never thought would make it :)

--- app/main.js
+++ app/main.js
@@ -18,7 +18,8 @@ let formSection = document.getElementById("form-section");
 let countdownSection = document.getElementById("countdown-section");
 let mainTitle = document.getElementById("title");
 //Date array
-let countdownElements = ["days", "hours", "minutes", "seconds"];
+let countdownElementsTop = ["#days", "#hours", "#minutes", "#seconds"];
+let countdownElementsBottom = ["#_days", "#_hours", "#_minutes", "#_seconds"];
 //Reset
 let resetDate = false;
 console.log(resetDate);
@@ -65,13 +66,15 @@ function countDown(date) {
 };
 //Filling cards
 function fillCard() {
-    for(let i = 0; i < countdownElements.length; i++) {
-        document.getElementById (countdownElements[i]).innerHTML=time[i];
+    for(let i = 0; i < countdownElementsTop.length; i++) {
+        document.querySelector(countdownElementsTop[i]).innerHTML=time[i];
+        document.querySelector(countdownElementsBottom[i]).innerHTML=time[i];
     }
-    //Adding a "0" before seconds, minutes and hours if they're < 10
-    for (let j = 1; j < countdownElements.length; j++) {
+    //Adding a "0" before seconds, minutes hours and days if they're < 10
+    for (let j = 0; j < countdownElementsTop.length; j++) {
         if (time [j] < 10) {
-            document.getElementById (countdownElements[j]).innerHTML="0"+time[j];
+            document.querySelector(countdownElementsTop[j]).innerHTML="0"+time[j];
+            document.querySelector(countdownElementsBottom[j]).innerHTML="0"+time[j];
         };
     };
 }
@@ -81,7 +84,6 @@ resetBtn.addEventListener('click', () => {
     console.log(resetDate);
     formSection.style.display = 'inherit';//Show again the form section
     countdownSection.style.display = 'none';//Hide the countdown
-    flipSeconds.classList.add('__flip-animation-inf');
     return resetDate;
 })
 //Time expired
@@ -90,40 +92,76 @@ function expired(x){
         clearInterval(x);//Finish countdown
 
         //Fill cards with 00s
-        document.getElementById(countdownElements[0]).innerHTML = '0'; //Days
-        for (let i = 1; i < countdownElements.length; i++){
-            document.getElementById(countdownElements[i]).innerHTML = '00'; //Hours, minutes, seconds
+        document.querySelector(countdownElementsTop[0]).innerHTML = '0'; //Days
+        document.querySelector(countdownElementsBottom[0]).innerHTML = '0'; //Days
+        for (let i = 1; i < countdownElementsTop.length; i++){
+            document.querySelector(countdownElementsTop[i]).innerHTML = '00'; //Hours, minutes, seconds
+            document.querySelector(countdownElementsBottom[i]).innerHTML = '00'; //Hours, minutes, seconds
         }
         //Change title content
         mainTitle.textContent = `EXPIRED`;
     }
 };
-//Animations
-let flipDays = document.getElementById("flip-days");
-let flipHours = document.getElementById("flip-hours");
-let flipMinutes = document.getElementById("flip-minutes");
-let flipSeconds = document.getElementById("flip-seconds");
+//Animations variables
+let flipDaysT = document.getElementById("flip-days-t");
+let flipDaysB = document.getElementById("flip-days-b");
+let spanDaysT = document.getElementById("days")
+let spanDaysB = document.getElementById("_days")
 
-function animation(){
-    if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
-        flipSeconds.classList.remove('__flip-animation-inf');
-    } else if (seconds === 0) {
-        flipMinutes.classList.add('__flip-animation');
-    } else if (minutes === 0 && seconds === 0) {
-        flipHours.classList.add('__flip-animation');
-    } else if (minutes === 0 && seconds === 0 && hours === 0) {
-        flipDays.classList.add('__flip-animation');
-    } else {
-        flipMinutes.classList.remove('__flip-animation');
-        flipHours.classList.remove('__flip-animation');
-        flipDays.classList.remove('__flip-animation');
-    };
+let flipHoursT = document.getElementById("flip-hours-t");
+let flipHoursB = document.getElementById("flip-hours-b");
+let spanHoursT = document.getElementById("hours")
+let spanHoursB = document.getElementById("_hours")
 
-// An issue with this animation code is, 
-//when you load the app, if any date is 0,
-//animations get loaded
-//Can I fix this with async?
+let flipMinutesT = document.getElementById("flip-minutes-t");
+let flipMinutesB = document.getElementById("flip-minutes-b");
+let spanMinutesT = document.getElementById("minutes")
+let spanMinutesB = document.getElementById("_minutes")
 
+let flipSecondsT = document.getElementById("flip-seconds-t");
+let flipSecondsB = document.getElementById("flip-seconds-b");
+let spanSecondsT = document.getElementById("seconds");
+let spanSecondsB = document.getElementById("_seconds");
+
+//Countdown animation
+function animateCountdown (topDiv, bottomDiv, topSpan, bottomSpan) {
+topDiv.style.animation = "flippinCardTop 0.5s linear";
+bottomDiv.style.animation = "flippinCardBottom 0.5s linear";
+topSpan.style.animation = "flippinCardTop 0.5s linear";
+bottomSpan.style.animation = "flippinCardBottom 0.5s linear";
+
+function animationTop() {
+    topDiv.style.animation = "";
+    topDiv.removeEventListener("animationend", animationTop);
+    topSpan.style.animation = "";
+    topSpan.removeEventListener("animationend", animationTop);
 };
 
+function animationBottom() {
+    bottomDiv.style.animation = "";
+    bottomDiv.removeEventListener("animationend", animationBottom);
+    bottomSpan.style.animation = "";
+    bottomSpan.removeEventListener("animationend", animationBottom);
+};
 
+topDiv.addEventListener("animationend", animationTop);
+bottomDiv.addEventListener("animationend", animationBottom);
+topSpan.addEventListener("animationend", animationTop);
+bottomSpan.addEventListener("animationend", animationBottom)
+}
+
+//Animation function
+function animation() {
+    if(seconds >=0) { //Seconds always animated (until EXPIRED)
+        animateCountdown(flipSecondsT, flipSecondsB, spanSecondsT, spanSecondsB)
+    }
+    if(seconds === 0 && distance !== 0) {
+        animateCountdown(flipMinutesT, flipMinutesB, spanMinutesT, spanMinutesB);
+    }
+    if(seconds === 0 && minutes === 0 && distance !== 0) {
+        animateCountdown(flipHoursT, flipHoursB, spanHoursT, spanHoursB);
+    }
+    if(seconds === 0 && minutes === 0 && hours === 0 && distance !== 0) {
+        animateCountdown(flipDaysT, flipDaysB, spanDaysT, spanDaysB);
+    }
+}
